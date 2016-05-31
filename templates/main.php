b8a979496aef25a009d84233ab3e7482649a6773
Ahmad Khudeish <ahmadkhudeish@gmail.com>
2016-06-01 11:34:47 +1200

    added geojson script

--- templates/main.php
+++ templates/main.php
@@ -1,10 +1,10 @@
-<script src="data/cities.geojson"></script>
+<script src="http://ec2-52-64-29-123.ap-southeast-2.compute.amazonaws.com/apps/home/js/cities.geojson"></script>
 
 <?php
 script('home', 'script');
 script('home', 'leaflet');
 script('home', 'leaflet-src');
-script('home', 'cities.geojson');
+// script('home', 'cities.geojson');
 
 
 style('home', 'style');
